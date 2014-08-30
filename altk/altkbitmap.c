efbaec6288f4e0249e609b2f9479871b8c5c3162
deglingo <ahmidmefes@gmail.com>
2014-08-30 22:05:35 +0200

    added the AltkBitmap class

---
+++ altk/altkbitmap.c
@@ -0,0 +1,14 @@
+/* altkbitmap.c -
+ */
+
+#include "altk/private.h"
+#include "altk/altkbitmap.h"
+#include "altk/altkbitmap.inl"
+
+
+
+/* altk_bitmap_class_init:
+ */
+static void altk_bitmap_class_init ( LObjectClass *cls )
+{
+}
---
+++ altk/altkbitmap.h
@@ -0,0 +1,30 @@
+/* altkbitmap.h -
+ */
+
+#ifndef _ALTKBITMAP_H_
+#define _ALTKBITMAP_H_
+
+#include "altk/altkdrawable.h"
+#include "altk/altkbitmap-def.h"
+
+
+
+/* AltkBitmap:
+ */
+struct _AltkBitmap
+{
+  ALTK_BITMAP_INSTANCE_HEADER;
+};
+
+
+
+/* AltkBitmapClass:
+ */
+struct _AltkBitmapClass
+{
+  ALTK_BITMAP_CLASS_HEADER;
+};
+
+
+
+#endif
