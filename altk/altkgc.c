46446f3b87128b0e2379d4bb35771a5c6df5c58a
deglingo <ahmidmefes@gmail.com>
2014-08-31 11:24:05 +0200

    added the AltkGC class

---
+++ altk/altkgc.c
@@ -0,0 +1,14 @@
+/* altkgc.c -
+ */
+
+#include "altk/private.h"
+#include "altk/altkgc.h"
+#include "altk/altkgc.inl"
+
+
+
+/* altk_gc_class_init:
+ */
+static void altk_gc_class_init ( LObjectClass *cls )
+{
+}
---
+++ altk/altkgc.h
@@ -0,0 +1,30 @@
+/* altkgc.h -
+ */
+
+#ifndef _ALTKGC_H_
+#define _ALTKGC_H_
+
+#include "altk/altkbase.h"
+#include "altk/altkgc-def.h"
+
+
+
+/* AltkGC:
+ */
+struct _AltkGC
+{
+  ALTK_GC_INSTANCE_HEADER;
+};
+
+
+
+/* AltkGCClass:
+ */
+struct _AltkGCClass
+{
+  ALTK_GC_CLASS_HEADER;
+};
+
+
+
+#endif
