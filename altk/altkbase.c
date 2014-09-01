0b33623b4f51c4aaa3d0732148c657fcdebd98b3
deglingo <ahmidmefes@gmail.com>
2014-09-01 15:57:27 +0200

    installed the mouse driver

--- altk/altkbase.c
+++ altk/altkbase.c
@@ -3,6 +3,7 @@
 
 #include "altk/private.h"
 #include "altk/altkbase.h"
+#include "altk/altkmain.h"
 
 
 
@@ -24,7 +25,10 @@ int altk_init ( void )
     CL_ERROR("could not initialize the image addon");
   if (!al_init_font_addon())
     CL_ERROR("could not initialize the font addon");
+  if (!al_install_mouse())
+    CL_ERROR("could not initialize the mouse driver");
   ALTK_QUARK_AL_OWNER = g_quark_from_string("altk-quark-al-owner");
   _altk_main_init();
+  altk_main_register_al_source(al_get_mouse_event_source());
   return 0;
 }
