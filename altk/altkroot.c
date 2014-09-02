4bfa878df624c716e76309a6ca9b5da9b0fbda36
deglingo <ahmidmefes@gmail.com>
2014-09-02 19:11:01 +0200

    added the AltkRoot class

---
+++ altk/altkroot.c
@@ -0,0 +1,14 @@
+/* altkroot.c -
+ */
+
+#include "altk/private.h"
+#include "altk/altkroot.h"
+#include "altk/altkroot.inl"
+
+
+
+/* altk_root_class_init:
+ */
+static void altk_root_class_init ( LObjectClass *cls )
+{
+}
