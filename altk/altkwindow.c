bacf837e4ba636714a0424887b500439161b1e9a
deglingo <ahmidmefes@gmail.com>
2014-09-03 10:44:03 +0200

    added the AltkWindow class

---
+++ altk/altkwindow.c
@@ -0,0 +1,14 @@
+/* altkwindow.c -
+ */
+
+#include "altk/private.h"
+#include "altk/altkwindow.h"
+#include "altk/altkwindow.inl"
+
+
+
+/* altk_window_class_init:
+ */
+static void altk_window_class_init ( LObjectClass *cls )
+{
+}
---
+++ altk/altkwindow.h
@@ -0,0 +1,30 @@
+/* altkwindow.h -
+ */
+
+#ifndef _ALTKWINDOW_H_
+#define _ALTKWINDOW_H_
+
+#include "altk/altkdrawable.h"
+#include "altk/altkwindow-def.h"
+
+
+
+/* AltkWindow:
+ */
+struct _AltkWindow
+{
+  ALTK_WINDOW_INSTANCE_HEADER;
+};
+
+
+
+/* AltkWindowClass:
+ */
+struct _AltkWindowClass
+{
+  ALTK_WINDOW_CLASS_HEADER;
+};
+
+
+
+#endif
