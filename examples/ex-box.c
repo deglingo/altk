317471f6f93241e6956ea86a932c1a92df8358ba
deglingo <ahmidmefes@gmail.com>
2014-10-02 18:55:46 +0200

    fixed ex-box

--- examples/ex-box.c
+++ examples/ex-box.c
@@ -11,15 +11,17 @@
 gint main ()
 {
   AltkDisplay *display;
-  AltkWidget *dlg, *top, *lbl;
+  AltkWidget *dlg, *top, *lbl1, *lbl2;
   CL_DEBUG("HELLO!");
   altk_init();
   display = altk_display_new();
   dlg = altk_dialog_new(display);
   top = altk_box_new();
   altk_container_add(ALTK_CONTAINER(dlg), top);
-  lbl = altk_label_new("Hello!");
-  altk_box_pack_start(ALTK_BOX(top), lbl, 0);
+  lbl1 = altk_button_new_with_label("Hello 1!");
+  altk_box_pack_start(ALTK_BOX(top), lbl1, 0);
+  lbl2 = altk_button_new_with_label("Hello 2!");
+  altk_box_pack_start(ALTK_BOX(top), lbl2, 0);
   altk_widget_show_all(dlg);
   altk_display_open(display);
   altk_main();
