8f2e2ab3507f4c1023e65d13421b857c32d91f69
deglingo <ahmidmefes@gmail.com>
2014-10-30 13:56:59 +0100

    fixed signal emission detail

--- altk/altkdisplay.c
+++ altk/altkdisplay.c
@@ -94,7 +94,7 @@ void altk_display_open ( AltkDisplay *display )
   /* create the root window */
   display->root_window = altk_window_new_root(display);
   /* signal */
-  l_signal_emit(L_OBJECT(display), signals[SIG_OPEN]);
+  l_signal_emit(L_OBJECT(display), signals[SIG_OPEN], 0);
 }
 
 
