8712862eb74f0e30c169449ed1b5a8ac21f1fdd0
deglingo <ahmidmefes@gmail.com>
2014-11-10 12:20:29 +0100

    fixed table layout

--- altk/altktable.c
+++ altk/altktable.c
@@ -15,6 +15,7 @@ typedef struct _LineLayout
   gint size_req;
   gint pos1;
   gint pos2;
+  gboolean expand;
 }
   LineLayout;
 
@@ -64,6 +65,8 @@ static LineLayout *layout_col ( Layout *layout,
                                 gint col );
 static LineLayout *layout_row ( Layout *layout,
                                 gint row );
+static gint layout_n_hexpands ( Layout *layout );
+static gint layout_n_vexpands ( Layout *layout );
 
 
 
@@ -76,7 +79,8 @@ typedef struct _Child
   gint left;
   gint bottom;
   gint right;
-  AltkPackFlags flags;
+  AltkPackFlags pack_flags;
+  AltkRequisition request;
 }
   Child;
 
@@ -104,8 +108,6 @@ static Layout *layout_new ( void )
   layout->left = 0;
   layout->bottom = 0;
   layout->right = 0;
-  layout->cols = g_new0(LineLayout, 16);
-  layout->rows = g_new0(LineLayout, 16);
   return layout;
 }
 
@@ -119,15 +121,29 @@ static void layout_extend ( Layout *layout,
                             gint bottom,
                             gint right )
 {
-  /* [TODO] */
-  ASSERT(top >= 0);
-  ASSERT(left >= 0);
-  ASSERT(right < 16);
-  ASSERT(bottom < 16);
-  layout->top = MIN(layout->top, top);
-  layout->left = MIN(layout->left, left);
-  layout->bottom = MAX(layout->bottom, bottom);
-  layout->right = MAX(layout->right, right);
+  gboolean xprows=FALSE, xpcols=FALSE;
+  /* rows */
+  if (top < layout->top) {
+    layout->top = top;
+    xprows = TRUE;
+  }
+  if (bottom > layout->bottom) {
+    layout->bottom = bottom;
+    xprows = TRUE;
+  }
+  if (xprows)
+    layout->rows = g_realloc(layout->rows, sizeof(LineLayout) * (layout->bottom - layout->top));
+  /* cols */
+  if (left < layout->left) {
+    layout->left = left;
+    xpcols = TRUE;
+  }
+  if (right > layout->right) {
+    layout->right = right;
+    xpcols = TRUE;
+  }
+  if (xpcols)
+    layout->cols = g_realloc(layout->cols, sizeof(LineLayout) * (layout->right - layout->left));
 }
 
 
@@ -152,7 +168,7 @@ static LineLayout *layout_col ( Layout *layout,
 {
   /* [FIXME] */
   if (col >= layout->left && col < layout->right)
-    return &layout->cols[col];
+    return &layout->cols[col - layout->left];
   else
     return NULL;
 }
@@ -166,9 +182,39 @@ static LineLayout *layout_row ( Layout *layout,
 {
   /* [FIXME] */
   if (row >= layout->top && row < layout->bottom)
-    return &layout->rows[row];
+    return &layout->rows[row - layout->top];
   else
     return NULL;
+}
+
+
+
+/* layout_n_hexpands:
+ */
+static gint layout_n_hexpands ( Layout *layout )
+{
+  gint n = 0;
+  gint l;
+  for (l = layout->left; l < layout->right; l++) {
+    if (layout_col(layout, l)->expand)
+      n++;
+  }
+  return n;
+}
+
+
+
+/* layout_n_vexpands:
+ */
+static gint layout_n_vexpands ( Layout *layout )
+{
+  gint n = 0;
+  gint l;
+  for (l = layout->top; l < layout->bottom; l++) {
+    if (layout_row(layout, l)->expand)
+      n++;
+  }
+  return n;
 }
 
 
@@ -225,15 +271,14 @@ static void _size_request ( AltkWidget *wid,
       Child *child = l->data;
       gint ncols, nrows;
       guint dsize;
-      AltkRequisition child_req;
       AltkSizeDistrib distrib;
       if (!ALTK_WIDGET_VISIBLE(child->widget))
         continue;
       /* CL_DEBUG("child request: %s", l_object_to_string(L_OBJECT(child->widget))); */
-      altk_widget_size_request(child->widget, &child_req);
+      altk_widget_size_request(child->widget, &child->request);
       /* columns layout */
       ncols = child->right - child->left;
-      dsize = child_req.width - priv->hpad * (ncols - 1);
+      dsize = child->request.width - priv->hpad * (ncols - 1);
       /* CL_DEBUG(" -> ncols=%d, dsize=%d", ncols, dsize); */
       if (dsize > 0)
         {
@@ -243,12 +288,14 @@ static void _size_request ( AltkWidget *wid,
               LineLayout *ll = layout_col(layout, i);
               gint dist_size = altk_size_distrib_next(&distrib);
               ll->size_req = MAX(ll->size_req, dist_size);
+              if (child->pack_flags & ALTK_PACK_HEXPAND)
+                ll->expand = TRUE;
               /* CL_DEBUG(" -> col[%d].size_req = %d", i, ll->size_req); */
             }
         }
       /* rows layout */
       nrows = child->bottom - child->top;
-      dsize = child_req.height - priv->vpad * (nrows - 1);
+      dsize = child->request.height - priv->vpad * (nrows - 1);
       /* CL_DEBUG(" -> nrows=%d, dsize=%d", nrows, dsize); */
       if (dsize > 0)
         {
@@ -258,6 +305,8 @@ static void _size_request ( AltkWidget *wid,
               LineLayout *ll = layout_row(layout, i);
               gint dist_size = altk_size_distrib_next(&distrib);
               ll->size_req = MAX(ll->size_req, dist_size);
+              if (child->pack_flags & ALTK_PACK_VEXPAND)
+                ll->expand = TRUE;
               /* CL_DEBUG(" -> row[%d].size_req = %d", i, ll->size_req); */
             }
         }
@@ -279,6 +328,49 @@ static void _size_request ( AltkWidget *wid,
 
 
 
+static void _alloc_child ( AltkWidget *wid,
+                           Child *child,
+                           gint x,
+                           gint y,
+                           gint width,
+                           gint height )
+{
+  AltkAllocation alloc;
+  if (width > child->request.width && !(child->pack_flags & ALTK_PACK_HFILL))
+    {
+      if (child->pack_flags & ALTK_PACK_ANCHOR_LEFT)
+        alloc.x = x;
+      else if (child->pack_flags & ALTK_PACK_ANCHOR_RIGHT)
+        alloc.x = x + (width - child->request.width);
+      else
+        alloc.x = x + (width - child->request.width) / 2;
+      alloc.width = child->request.width;
+    }
+  else
+    {
+      alloc.x = x;
+      alloc.width = width;
+    }
+  if (height > child->request.height && !(child->pack_flags & ALTK_PACK_VFILL))
+    {
+      if (child->pack_flags & ALTK_PACK_ANCHOR_TOP)
+        alloc.y = y;
+      else if (child->pack_flags & ALTK_PACK_ANCHOR_BOTTOM)
+        alloc.y = y + (height - child->request.height);
+      else
+        alloc.y = y + (height - child->request.height) / 2;
+      alloc.height = child->request.height;
+    }
+  else
+    {
+      alloc.y = y;
+      alloc.height = height;
+    }
+  altk_widget_size_allocate(child->widget, &alloc);
+}
+
+
+
 /* _size_allocate:
  */
 static void _size_allocate ( AltkWidget *wid,
@@ -289,7 +381,31 @@ static void _size_allocate ( AltkWidget *wid,
   Layout *layout = priv->layout;
   gint i, ofs;
   GList *l;
+  AltkSizeDistrib hdistrib, vdistrib;
+  gboolean hgrow=FALSE, vgrow=FALSE, hshrink=FALSE, vshrink=FALSE;
   ALTK_WIDGET_CLASS(parent_class)->size_allocate(wid, alloc);
+  /* hgrow/shrink */
+  if (alloc->width > wid->size_request.width)
+    {
+      hgrow = TRUE;
+      altk_size_distrib_init(&hdistrib, alloc->width - wid->size_request.width, layout_n_hexpands(layout));
+    }
+  else if (alloc->width < wid->size_request.width)
+    {
+      hshrink = TRUE;
+      altk_size_distrib_init(&hdistrib, wid->size_request.width - alloc->width, layout->right - layout->left);
+    }
+  /* vgrow/shrink */
+  if (alloc->height > wid->size_request.height)
+    {
+      vgrow = TRUE;
+      altk_size_distrib_init(&vdistrib, alloc->height - wid->size_request.height, layout_n_vexpands(layout));
+    }
+  else if (alloc->height < wid->size_request.height)
+    {
+      vshrink = TRUE;
+      altk_size_distrib_init(&vdistrib, wid->size_request.height - alloc->height, layout->bottom - layout->top);
+    }
   /* prepare cols/rows */
   ofs = 0;
   for (i = layout->left; i < layout->right; i++)
@@ -297,6 +413,10 @@ static void _size_allocate ( AltkWidget *wid,
       LineLayout *ll = layout_col(layout, i);
       ll->pos1 = ofs;
       ll->pos2 = ofs + ll->size_req;
+      if (hgrow && ll->expand)
+        ll->pos2 += altk_size_distrib_next(&hdistrib);
+      else if (hshrink)
+        ll->pos2 -= altk_size_distrib_next(&hdistrib);
       ofs = ll->pos2 + priv->hpad;
     }
   ofs = 0;
@@ -305,22 +425,26 @@ static void _size_allocate ( AltkWidget *wid,
       LineLayout *ll = layout_row(layout, i);
       ll->pos1 = ofs;
       ll->pos2 = ofs + ll->size_req;
+      if (vgrow && ll->expand)
+        ll->pos2 += altk_size_distrib_next(&vdistrib);
+      else if (vshrink)
+        ll->pos2 -= altk_size_distrib_next(&vdistrib);
       ofs = ll->pos2 + priv->vpad;
     }
   /* alloc children */
   for (l = priv->children; l; l = l->next)
     {
       Child *child = l->data;
-      AltkAllocation child_alloc;
+      gint x, y, width, height;
       if (!ALTK_WIDGET_VISIBLE(child->widget))
         continue;
-      child_alloc.x = layout_col(layout, child->left)->pos1;
-      child_alloc.y = layout_row(layout, child->top)->pos1;
-      child_alloc.width = layout_col(layout, child->right - 1)->pos2 - child_alloc.x;
-      child_alloc.height = layout_row(layout, child->bottom - 1)->pos2 - child_alloc.y;
-      child_alloc.x += alloc->x;
-      child_alloc.y += alloc->y;
-      altk_widget_size_allocate(child->widget, &child_alloc);
+      x = layout_col(layout, child->left)->pos1;
+      y = layout_row(layout, child->top)->pos1;
+      width = layout_col(layout, child->right - 1)->pos2 - x;
+      height = layout_row(layout, child->bottom - 1)->pos2 - y;
+      x += alloc->x;
+      y += alloc->y;
+      _alloc_child(wid, child, x, y, width, height);
     }
 }
 
@@ -390,6 +514,6 @@ void altk_table_attach ( AltkTable *table,
   tchild->left = left;
   tchild->bottom = bottom;
   tchild->right = right;
-  tchild->flags = flags;
+  tchild->pack_flags = flags;
   layout_extend(PRIVATE(table)->layout, top, left, bottom, right);
 }
